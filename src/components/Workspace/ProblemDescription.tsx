e280db739b0564c88b0400a974a1d91a1effe23a
Anton Mamontov <anton_mamont@bk.ru>
2023-12-18 11:24:47 +0000

    fetch problem statistics from the DB and add skeletons on problem page loading

--- src/components/Workspace/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription.tsx
@@ -1,15 +1,19 @@
-import { LocalProblem } from '@/utils/types/problem';
-import React from 'react';
+import { firestore } from '@/firebase/firebase';
+import { DbProblem, LocalProblem } from '@/utils/types/problem';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { AiFillDislike, AiFillLike } from 'react-icons/ai';
 import { BsCheck2Circle } from 'react-icons/bs';
 import { TiStarOutline } from 'react-icons/ti';
+import RectangleSkeleton from '../Skeletons/RectangleSkeleton';
+import CircleSkeleton from '../Skeletons/CircleSkeleton';
 
 type ProblemDescriptionProps = {
     problem: LocalProblem
 };
 
 const ProblemDescription:React.FC<ProblemDescriptionProps> = ({problem}) => {
-    
+    const {currentProblem : problemStatistics, loading, problemDifficultyClass} = useGetCurrentProblem(problem.id);
     return <div className='bg-dark-layer-1'>
         <div className='flex h-11 w-full items-center pt-2 bg-dark-layer-2 text-white overflow-x-hidden'>
             <div className='bg-dark-layer-1 rounded-t-[5px] px-5 py-[10px] text-xs cursor-pointer'>
@@ -24,23 +28,32 @@ const ProblemDescription:React.FC<ProblemDescriptionProps> = ({problem}) => {
                         <div className='flex-1 mr-2 text-lg text-white font-medium'>{problem.title}</div>
                     </div>
                 </div>
-                <div className='flex items-center mt-3'>
-                    <div className='text-olive bg-olive inline-block rounded-[21px] bg-opacity-[.15] px-2.5 py-1 text-xs font-medium capitalize'>easy</div>
+                {loading && (
+                    <div className='mt-3 flex space-x-2'>
+                        <RectangleSkeleton/>
+                        <CircleSkeleton/>
+                        <RectangleSkeleton/>
+                        <RectangleSkeleton/>
+                    </div>
+                )}
+                {!loading && problemStatistics && <div className='flex items-center mt-3'>
+                    <div className={`${problemDifficultyClass} inline-block rounded-[21px] bg-opacity-[.15] px-2.5 py-1 text-xs font-medium capitalize`}>{problemStatistics?.difficulty}</div>
                     <div className='rounded p-[3x] ml-4 text-lg transition-colors duration-200 text-dark-green-s'>
                         <BsCheck2Circle/>
                     </div>
                     <div className='flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-dark-gray-6'>
                         <AiFillLike/>
-                        <span className='text-xs'>120</span>
+                        <span className='text-xs'>{problemStatistics.likes}</span>
                     </div>
                     <div className='flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-dark-gray-6'>
                         <AiFillDislike/>
-                        <span className='text-xs'>2</span>
+                        <span className='text-xs'>{problemStatistics.disLikes}</span>
                     </div>
                     <div className='cursor-pointer hover:bg-dark-fill-3 rounded p-[3px] ml-4 text-xl transition-colors duration-200 text-dark-gray-6'>
                         <TiStarOutline/>
                     </div>
                 </div>
+                }
 
                 {/* Problem statement */}
                 <div className='text-white text-sm'>
@@ -81,3 +94,33 @@ const ProblemDescription:React.FC<ProblemDescriptionProps> = ({problem}) => {
     </div>
 }
 export default ProblemDescription;
+
+function useGetCurrentProblem(problemId:string) {
+    const [currentProblem, setCurrentProblem] = useState<DbProblem | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [problemDifficultyClass, setproblemDifficultyClass] = useState<string>('');
+
+    useEffect(() => {
+        // get problem from DB
+        const getCurrentProblem = async () => {
+            setLoading(true);
+            const docRef = doc(firestore, "problems", problemId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                const problem = docSnap.data();
+                setCurrentProblem({id:docSnap.id, ...problem} as DbProblem);
+
+                setproblemDifficultyClass(
+                    problem.difficulty === "Easy" ? "bg-olive  text-olive" : 
+                    problem.difficulty === "Medium" ? "bg-dark-yellow text-dark-yellow" :
+                    problem.difficulty === "Hard" ? "bg-dark-pink text-dark-pink" : ""
+                )
+            }
+            setLoading(false);
+        }
+
+        getCurrentProblem();
+    }, [problemId]) 
+
+    return {currentProblem, loading, problemDifficultyClass}
+}
