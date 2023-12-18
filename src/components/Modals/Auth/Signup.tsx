756a9864c06132f4d671c28e2e88c368cba4add8
Anton Mamontov <anton_mamont@bk.ru>
2023-12-18 11:23:36 +0000

    create user record on user registration

--- src/components/Modals/Auth/Signup.tsx
+++ src/components/Modals/Auth/Signup.tsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useSetRecoilState} from 'recoil'
 import {AuthModalState, authModalState } from '@/atoms/authModalAtom';
-import { auth } from '@/firebase/firebase';
+import { auth, firestore } from '@/firebase/firebase';
 import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
+import { doc, setDoc } from 'firebase/firestore';
 
 type SignupProps = {
     
@@ -36,12 +37,30 @@ const Signup:React.FC<SignupProps> = () => {
             return;
         }
         try {
+            toast.loading("Creating your account", {
+                position: 'top-center',
+                theme:'dark',
+                toastId: 'accountCreationToast'
+            })
             const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
             if (!newUser) return;
-
+            const userData = {
+                uid: newUser.user.uid,
+                email: newUser.user.email,
+                displayName: inputs.displayName,
+                createdAt: Date.now(),
+                updatedAt: Date.now(),
+                likedProblems: [],
+                dislikedProblems: [],
+                solvedProblems: [],
+                starredProblems: []
+            }
+            await setDoc(doc(firestore, "users", newUser.user.uid), userData)
             router.push('/')
         } catch (error: any) {
-            toast.error(error.message, {position: 'top-center', autoClose: 3000, theme:'dark'});
+            toast.error(error.message, {position: 'top-center', autoClose: 2000, theme:'dark'});
+        } finally {
+            toast.dismiss('accountCreationToast');
         }
     };
 
