95e29330e329c524e97fc0697adb77d1aeca4132
Anton Mamontov <anton_mamont@bk.ru>
2023-12-19 04:41:30 +0000

    resolve typescript error

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -41,22 +41,26 @@ const Playground:React.FC<PlaygroundProps> = ({problem, setSuccess, setSolved}) 
         try {
             userCode = userCode.slice(userCode.indexOf(problem.starterFunctionName));
             const cb = new Function(`return ${userCode}`)();
-            const success = problems[pid as string].handlerFunction(cb);
-            if (success) {
-                const userRef = doc(firestore, "users", user.uid);
-                await updateDoc(userRef, {
-                    solvedProblems: arrayUnion(pid)
-                });
-                toast.success("Congrats! All tests passed!!", {
-                    position:"top-center",
-                    theme: 'dark',
-                    autoClose: 3000
-                });
-                setSolved(true);
-                setSuccess(true);
-                setTimeout(() => {
-                    setSuccess(false);
-                }, 4000)
+            const handler = problems[pid as string].handlerFunction;
+
+            if (typeof handler === "function") {
+                const success = handler(cb);
+                if (success) {
+                    const userRef = doc(firestore, "users", user.uid);
+                    await updateDoc(userRef, {
+                        solvedProblems: arrayUnion(pid)
+                    });
+                    toast.success("Congrats! All tests passed!!", {
+                        position:"top-center",
+                        theme: 'dark',
+                        autoClose: 3000
+                    });
+                    setSolved(true);
+                    setSuccess(true);
+                    setTimeout(() => {
+                        setSuccess(false);
+                    }, 4000)
+                }
             }
         } catch (error) {
             if (error instanceof Error) {
